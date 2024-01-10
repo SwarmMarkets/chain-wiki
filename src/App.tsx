606e8d9715d6ab4cb1c341befd05fccfc3675fc5
Arsen Lulukian <arsen.lulukyan@gmail.com>
2024-01-10 15:00:13 +0200

    add styled-components theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 import HomePage from './pages/HomePage'
 import ProjectPage from './pages/ProjectPage'
 import RoutePaths from './shared/enums/routes-paths'
 import Layout from './components/common/Layout'
 import ArticlePage from './pages/ArticlePage'
+import theme from './theme';
 
 function App() {
   return (
-    <Router>
-      <Layout>
-      <Routes>
-        <Route path={RoutePaths.HOME} element={<HomePage />} />
-        <Route path={RoutePaths.PROJECT} element={<ProjectPage />}>
-          <Route path={RoutePaths.PROJECT + RoutePaths.ARTICLE} element={<ArticlePage />} />
-        </Route>
-
-      </Routes>
-      </Layout>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Layout>
+        <Routes>
+          <Route path={RoutePaths.HOME} element={<HomePage />} />
+          <Route path={RoutePaths.PROJECT} element={<ProjectPage />}>
+            <Route path={RoutePaths.PROJECT + RoutePaths.ARTICLE} element={<ArticlePage />} />
+          </Route>
+        </Routes>
+        </Layout>
+      </Router>
+    </ThemeProvider>
   )
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,25 @@
+const theme = {
+  palette: {
+    // Text Colors
+    textColor: '#212121',
+    linkColor: '#3366cc',
+
+    // Background colors
+    bgColor: '#f8f9fa',
+
+    // Border colors
+    borderColor: '#ccc',
+  },
+  fontSizes: {
+    small: '12px',
+    medium: '14px',
+    large: '24px',
+  },
+  fontWeights: {
+    regular: 400,
+    bold: 700,
+  },
+};
+
+export default theme;
+export type Theme = typeof theme;
