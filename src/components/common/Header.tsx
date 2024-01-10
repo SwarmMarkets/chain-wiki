001b249275333d586ff3adccd8d70ab561ec9345
Arsen Lulukian <arsen.lulukyan@gmail.com>
2024-01-10 23:59:15 +0200

    add header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,17 +1,21 @@
 import styled from 'styled-components';
 import Container from '../ui/Container';
+import logo from '@src/assets/logo.png';
+import { Link } from 'react-router-dom';
 
 const HeaderContainer = styled(Container)`
-  padding-top: 8px;
-  padding-bottom: 8px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 12px;
+  padding-bottom: 12px;
 `;
 
 const Header = () => {
   return (
-    <HeaderContainer>
-      <header>
-        <h1>ChainWiki</h1>
-      </header>
+    <HeaderContainer as="header">
+      <img src={logo} alt="ChainWiki" />
+      <Link to="/">Log in</Link>
     </HeaderContainer>
   );
 };
