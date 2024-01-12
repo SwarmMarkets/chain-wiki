4e2aca669fa44d7dac82f664830269de6ca35cf6
Arsen Lulukian <arsen.lulukyan@gmail.com>
2024-01-12 22:52:31 +0200

    add Tabs component

---
+++ src/components/ui/Tabs.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+interface TabProps {
+  active: boolean;
+}
+
+interface Tab {
+  title: string;
+  content: React.ReactNode;
+}
+
+interface TabsProps {
+  tabs: Tab[];
+}
+
+const TabsWrapper = styled.div`
+  display: flex;
+  justify-content: end;
+  border-bottom: 1px solid ${({ theme }) => theme.palette.borderPrimary};
+`;
+
+const Tab = styled.div<TabProps>`
+  padding: 10px 15px;
+  cursor: pointer;
+  border-bottom: ${(props) =>
+    props.active ? `2px solid ${props.theme.palette.borderBlue}` : 'none'};
+  color: ${(props) =>
+    props.active
+      ? props.theme.palette.borderBlue
+      : props.theme.palette.textPrimary};
+`;
+
+const TabContent = styled.div`
+  padding: 15px 0;
+`;
+
+const Tabs: React.FC<TabsProps> = ({ tabs }) => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleTabClick = (index: number) => {
+    setActiveTab(index);
+  };
+
+  return (
+    <div>
+      <TabsWrapper>
+        {tabs.map((tab, index) => (
+          <Tab
+            key={index}
+            active={index === activeTab}
+            onClick={() => handleTabClick(index)}
+          >
+            {tab.title}
+          </Tab>
+        ))}
+      </TabsWrapper>
+      <TabContent>{tabs[activeTab].content}</TabContent>
+    </div>
+  );
+};
+
+export default Tabs;
