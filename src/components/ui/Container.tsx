f1fdb3fd7e75cb2fa4e96553342749971e3776fc
b4dPot4to <112569797+b4dPot4to@users.noreply.github.com>
2024-01-22 23:55:28 +0200

    update Container props

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -1,10 +1,17 @@
-import styled from 'styled-components';
+import { Theme } from '@src/theme'
+import styled from 'styled-components'
 
-const Container = styled.div`
-  max-width: 1492px;
+type MaxWidth = Exclude<keyof Theme['breakpoints'], 'xs'>
+
+interface ContainerProps {
+  $maxWidth?: MaxWidth
+}
+
+const Container = styled.div<ContainerProps>`
+  max-width: ${({ theme, $maxWidth = 'xl' }) => theme.breakpoints[$maxWidth]}px;
   margin: 0 auto;
   padding: 0 52px;
   background: ${({ theme }) => theme.palette.white};
-`;
+`
 
-export default Container;
+export default Container
