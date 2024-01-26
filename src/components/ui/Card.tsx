0d75509b2939ad7155a6f23a267110efa10d1479
Arsen Lulukian <arsen.lulukyan@gmail.com>
2024-01-26 18:36:11 +0200

    update Card

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -2,21 +2,41 @@ import { Link } from 'react-router-dom';
 import styled, { useTheme } from 'styled-components';
 import Text from './Text';
 import { ChildrenProp } from '@src/shared/types/common-props';
+import shouldForwardProp from '@styled-system/should-forward-prop';
+import {
+  FlexboxProps,
+  LayoutProps,
+  SpaceProps,
+  flexbox,
+  layout,
+  space,
+} from 'styled-system';
 
-interface CardProps extends ChildrenProp {
+interface CardProps
+  extends ChildrenProp,
+    FlexboxProps,
+    LayoutProps,
+    SpaceProps {
   title?: string;
   to?: string;
 }
 
-const CardRoot = styled.div`
+const CardRoot = styled.div.withConfig({
+  shouldForwardProp,
+})`
   padding: 16px;
   border: 1px solid ${({ theme }) => theme.palette.borderPrimary};
   border-radius: 10px;
   background: ${({ theme }) => theme.palette.bgPrimary};
+  ${flexbox}
+  ${layout}
+  ${space}
 `;
 
-const ChildrenWrapper = styled.div`
-  margin-top: 10px;
+const ChildrenWrapper = styled.div.withConfig({
+  shouldForwardProp,
+})<SpaceProps>`
+  ${space}
 `;
 
 const StyledLink = styled(Link)`
@@ -28,11 +48,11 @@ const StyledLink = styled(Link)`
   }
 `;
 
-const Card: React.FC<CardProps> = ({ title, to, children }) => {
+const Card: React.FC<CardProps> = ({ title, to, children, ...props }) => {
   const theme = useTheme();
 
   return (
-    <CardRoot>
+    <CardRoot {...props}>
       {to ? (
         <StyledLink to={to}>
           <Text.h2
@@ -51,7 +71,7 @@ const Card: React.FC<CardProps> = ({ title, to, children }) => {
           {title}
         </Text.h2>
       )}
-      {children && <ChildrenWrapper>{children}</ChildrenWrapper>}
+      {children && <ChildrenWrapper mt={title ? 10 : 0} >{children}</ChildrenWrapper>}
     </CardRoot>
   );
 };
