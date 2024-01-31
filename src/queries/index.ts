5201a1a1a3ffb25c32eb9fa5be8885cd2fe688c6
b4dPot4to <112569797+b4dPot4to@users.noreply.github.com>
2024-01-31 15:09:54 +0200

    add NFT gql schemas

---
+++ src/queries/index.ts
@@ -0,0 +1,5 @@
+import { loader } from 'graphql.macro' // TO DOWNLOAD
+
+export const NFTs = loader('./NFTs.gql')
+export const NFT = loader('./NFT.gql')
+export const NFTUpdates = loader('./NFTUpdates.gql')
