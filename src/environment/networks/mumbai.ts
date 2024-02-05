9bf2f7d47b08daeba11417f13d3dee864d52de52
b4dPot4to <112569797+b4dPot4to@users.noreply.github.com>
2024-02-05 19:36:51 +0200

    add supported chain ids; update utils index file

--- src/environment/networks/mumbai.ts
+++ src/environment/networks/mumbai.ts
@@ -1,4 +1,5 @@
 import { NetworkConfiguration } from "@src/shared/types/network-configuration";
+import { Mumbai } from "@thirdweb-dev/chains";
 
 export const mumbai: NetworkConfiguration = {
   subgraphURL: 'https://api.studio.thegraph.com/proxy/46523/chain-wiki/version/latest',
@@ -6,3 +7,5 @@ export const mumbai: NetworkConfiguration = {
     sx1155NFTFactoryAddress: '0xdEA790E5888B04004b6890a8DBAcd07358006273'
   }
 }
+
+export const mumbaiChainConfig = Mumbai
