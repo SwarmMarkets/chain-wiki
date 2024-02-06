e32728e861c8b3d028da7a98b83d8c11be2dbac6
Arsen Lulukian <85998249+basya01@users.noreply.github.com>
2024-02-06 21:57:05 +0200

    update project skeleton height

--- src/components/Project/ProjectSkeleton.tsx
+++ src/components/Project/ProjectSkeleton.tsx
@@ -7,13 +7,13 @@ const ProjectSkeleton = (
   <ContentLoader
     speed={2}
     width='100%'
-    height={188}
-    viewBox='0 0 100% 188'
+    height={248}
+    viewBox='0 0 100% 248'
     backgroundColor='#f3f3f3'
     foregroundColor='#ecebeb'
     {...props}
   >
-    <rect x='0' y='0' rx='10' ry='10' width='100%' height='188' />
+    <rect x='0' y='0' rx='10' ry='10' width='100%' height='248' />
   </ContentLoader>
 )
 
