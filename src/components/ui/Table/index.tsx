df50132c8896ef441c7cd49d8eb74e18093ffa7b
b4dPot4to <112569797+b4dPot4to@users.noreply.github.com>
2024-02-08 18:31:12 +0200

    add styled Table components

---
+++ src/components/ui/Table/index.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import { ColorProps, LayoutProps, SpaceProps, TypographyProps, color, layout, space, typography } from 'styled-system';
+
+// Extend styled-system props to use with styled-components
+type TableProps = SpaceProps & LayoutProps;
+type TableCellProps = TypographyProps & SpaceProps & ColorProps;
+
+// Table container that applies spacing and layout styles from styled-system
+export const Table = styled.table<TableProps>`
+  ${space}
+  ${layout}
+  width: 100%;
+  border-collapse: collapse;
+  border: 1px solid #e1e1e1;
+`;
+
+// TableRow with hover effect
+export const TableRow = styled.tr`
+  &:hover {
+    background-color: #f5f5f5;
+  }
+`;
+
+// TableHeader with improved typography and spacing
+export const TableHeader = styled.th<TableCellProps>`
+  ${typography}
+  ${space}
+  ${color}
+  text-align: left;
+  background-color: #fafafa;
+  border-bottom: 2px solid #e1e1e1;
+  padding: 12px 15px;
+`;
+
+// TableCell with consistent styling and padding
+export const TableCell = styled.td<TableCellProps>`
+  ${typography}
+  ${space}
+  ${color}
+  padding: 12px 15px;
+  border-bottom: 1px solid #e1e1e1;
+`;
