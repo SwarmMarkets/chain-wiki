1b31b07777caa1baddcd6362c157b9f9b4f643a5
b4dPot4to <112569797+b4dPot4to@users.noreply.github.com>
2024-02-23 17:33:47 +0200

    add success modal screens

---
+++ src/components/ui/SuccessScreens/SuccessModal.tsx
@@ -0,0 +1,35 @@
+import { BasicModalProps } from '@src/shared/types/common-props'
+import Modal from '../../ui/Modal'
+import SuccessContent from './SuccessContent'
+
+export type ContentType = 'project' | 'article'
+
+interface SuccessModalProps extends BasicModalProps {
+  title: string
+  description: string
+}
+
+const SuccessModal: React.FC<SuccessModalProps> = ({
+  title = 'Success',
+  description = '',
+  onClose,
+  isOpen,
+}) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      maxWidth='500px'
+      minHeight='200px'
+      width='100%'
+    >
+      <SuccessContent
+        title={title}
+        description={description}
+        onClick={onClose}
+      />
+    </Modal>
+  )
+}
+
+export default SuccessModal
---
+++ src/components/ui/SuccessScreens/types.ts
@@ -0,0 +1,4 @@
+export interface SuccessProps {
+  title: string
+  description: string
+}
