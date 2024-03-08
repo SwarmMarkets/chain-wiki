5575fad00afb9d0eef7ce4dfa0ec48447e22b445
Arsen Lulukian <arsen.lulukyan@gmail.com>
2024-03-08 11:35:15 +0200

    add useYupValidationResolver hook

---
+++ src/hooks/useYupValidationResolvber.ts
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useCallback } from 'react'
+import { ObjectSchema } from 'yup'
+
+interface Error {
+  type: string
+  message: string
+  path: string
+}
+
+interface Errors {
+  inner: Error[]
+}
+
+const useYupValidationResolver = (validationSchema: ObjectSchema<any>) =>
+  useCallback(
+    async (data: any) => {
+      console.log(data)
+      try {
+        const values = await validationSchema.validate(data, {
+          abortEarly: false,
+        })
+
+        return {
+          values,
+          errors: {},
+        }
+      } catch (errors) {
+        return {
+          values: {},
+          errors: (errors as Errors).inner.reduce(
+            (allErrors, currentError) => ({
+              ...allErrors,
+              [currentError.path]: {
+                type: currentError.type ?? 'validation',
+                message: currentError.message,
+              },
+            }),
+            {}
+          ),
+        }
+      }
+    },
+    [validationSchema]
+  )
+
+export default useYupValidationResolver
