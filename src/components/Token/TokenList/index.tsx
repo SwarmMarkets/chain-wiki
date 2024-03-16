129d8651bb597bff5be8d7bec82d9d56b45c9027
Arsen Lulukian <85998249+basya01@users.noreply.github.com>
2024-03-16 13:12:51 +0200

    remove ContentMissing component from TokenList

--- src/components/Token/TokenList/index.tsx
+++ src/components/Token/TokenList/index.tsx
@@ -1,6 +1,5 @@
 import { TokensQueryFullData } from '@src/shared/types/ipfs'
 import { useParams } from 'react-router-dom'
-import ContentMissing from '../../common/ContentMissing'
 import RequirePermissions from '../../common/RequirePermissions'
 import Flex from '../../ui/Flex'
 import TokenCardSkeleton from '../TokenCardSkeleton'
@@ -20,25 +19,20 @@ const TokenList: React.FC<TokenListProps> = ({
 }) => {
   const { nftId = '' } = useParams()
 
-  const noContent = !loading && (tokens?.length === 0 || !tokens)
-
   return (
     <Flex flexDirection='column' $gap='10px'>
       <RequirePermissions canCreateToken nftAddress={nftId}>
         <CreateTokenCard nftAddress={nftAddress} />
       </RequirePermissions>
-      {noContent ? (
-        <ContentMissing message='Tokens missing' />
-      ) : (
-        tokens?.map(token => (
-          <TokenCard
-            key={token.id}
-            tokenId={token.id}
-            nftId={nftId}
-            content={token.ipfsContent}
-          />
-        ))
-      )}
+
+      {tokens?.map(token => (
+        <TokenCard
+          key={token.id}
+          tokenId={token.id}
+          nftId={nftId}
+          content={token.ipfsContent}
+        />
+      ))}
 
       {loading &&
         [...new Array(5)].map((_, index) => <TokenCardSkeleton key={index} />)}
