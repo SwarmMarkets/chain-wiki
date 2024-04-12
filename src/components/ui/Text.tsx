8318a860ea363fb388a8bf3c4b37778575ea8e54
Arsen Lulukian <85998249+basya01@users.noreply.github.com>
2024-04-12 22:02:26 +0300

    fix formatting

--- src/components/ui/Text.tsx
+++ src/components/ui/Text.tsx
@@ -2,7 +2,20 @@ import styled from 'styled-components'
 
 import { ChildrenProp } from '@src/shared/types/common-props'
 import shouldForwardProp from '@styled-system/should-forward-prop'
-import { FontSizeProps, FontStyleProps, FontWeightProps, LineHeightProps, SpaceProps, TextAlignProps, fontSize, fontStyle, fontWeight, lineHeight, space, textAlign } from 'styled-system'
+import {
+  FontSizeProps,
+  FontStyleProps,
+  FontWeightProps,
+  LineHeightProps,
+  SpaceProps,
+  TextAlignProps,
+  fontSize,
+  fontStyle,
+  fontWeight,
+  lineHeight,
+  space,
+  textAlign,
+} from 'styled-system'
 
 interface StyledTextProps extends ChildrenProp {
   $color?: string
@@ -10,7 +23,9 @@ interface StyledTextProps extends ChildrenProp {
   $weight?: number
 }
 
-const StyledText = styled.span.withConfig({ shouldForwardProp })<StyledTextProps>`
+const StyledText = styled.span.withConfig({
+  shouldForwardProp,
+})<StyledTextProps>`
   color: ${props => props.$color || props.theme.palette.textPrimary};
   font-size: ${props => props.$size};
   font-weight: ${props => props.$weight};
@@ -37,21 +52,34 @@ interface TextProps
   weight?: number
 }
 
-const Text = ({ as: HTMLElement = 'span', color, size, weight, children, ...props }: TextProps) => {
+const Text = ({
+  as: HTMLElement = 'span',
+  color,
+  size,
+  weight,
+  children,
+  ...props
+}: TextProps) => {
   return (
-    <StyledText as={HTMLElement} $color={color} $size={size} $weight={weight} {...props}>
+    <StyledText
+      as={HTMLElement}
+      $color={color}
+      $size={size}
+      $weight={weight}
+      {...props}
+    >
       {children}
     </StyledText>
   )
 }
 
-Text.h1 = (props: TextProps) => <Text as="h1" {...props} />
-Text.h2 = (props: TextProps) => <Text as="h2" {...props} />
-Text.h3 = (props: TextProps) => <Text as="h3" {...props} />
-Text.h4 = (props: TextProps) => <Text as="h4" {...props} />
-Text.h5 = (props: TextProps) => <Text as="h5" {...props} />
-Text.h6 = (props: TextProps) => <Text as="h6" {...props} />
-Text.span = (props: TextProps) => <Text as="span" {...props} />
-Text.p = (props: TextProps) => <Text as="p" {...props} />
+Text.h1 = (props: TextProps) => <Text as='h1' {...props} />
+Text.h2 = (props: TextProps) => <Text as='h2' {...props} />
+Text.h3 = (props: TextProps) => <Text as='h3' {...props} />
+Text.h4 = (props: TextProps) => <Text as='h4' {...props} />
+Text.h5 = (props: TextProps) => <Text as='h5' {...props} />
+Text.h6 = (props: TextProps) => <Text as='h6' {...props} />
+Text.span = (props: TextProps) => <Text as='span' {...props} />
+Text.p = (props: TextProps) => <Text as='p' {...props} />
 
 export default Text
