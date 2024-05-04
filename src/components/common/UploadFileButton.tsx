26173255a4f0fd1a6462232793f6dfbf5307c19a
Arsen Lulukian <arsen@swarm.com>
2024-05-04 21:45:31 +0300

    Update UploadFileButton.tsx

--- src/components/common/UploadFileButton.tsx
+++ src/components/common/UploadFileButton.tsx
@@ -58,6 +58,7 @@ const UploadFileButton: React.FC<UploadFileButtonProps> = ({
         type='button'
         onClick={e => {
           e.stopPropagation()
+          e.preventDefault()
           inputRef.current?.click()
         }}
       >
