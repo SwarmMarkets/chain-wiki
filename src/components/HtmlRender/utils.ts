9a2ac298051862282fe4183cd583687494ac3a8c
Zhuk Evheny <113713601+Beatles93@users.noreply.github.com>
2024-09-16 20:20:32 +0300

    feat: partly add count

--- src/components/HtmlRender/utils.ts
+++ src/components/HtmlRender/utils.ts
@@ -1,22 +1,56 @@
 import CommentIcon from '@src/assets/icons/comment.svg'
+import { CommentIdsQuery } from '@src/queries'
+import client from '@src/services/apollo'
 
-export const createCommentIconElement = () => {
+const getCountOfAttestations = async (sectionId: string): Promise<number> => {
+  try {
+    const { data } = await client.query({
+      query: CommentIdsQuery,
+      variables: { sectionId, limit: 50 },
+    })
+    return data.comments.length
+  } catch (error) {
+    console.error('Error fetching comments count:', error)
+    return 0
+  }
+}
+
+export const createCommentIconElement = async (sectionId: string) => {
+  const wrapperElem = document.createElement('div')
+  wrapperElem.style.position = 'relative'
+  wrapperElem.style.display = 'none'
+  wrapperElem.style.top = '0'
+  wrapperElem.style.right = '0'
+
   const commentIconElem = document.createElement('img')
   commentIconElem.setAttribute('type', 'image/svg+xml')
   commentIconElem.setAttribute('src', CommentIcon)
-
   commentIconElem.style.width = '22px'
   commentIconElem.style.height = '22px'
   commentIconElem.style.cursor = 'pointer'
-  
+  wrapperElem.style.position = 'absolute'
+  wrapperElem.style.top = '0'
+  wrapperElem.style.right = '0'
   commentIconElem.style.background = 'white'
   commentIconElem.style.padding = '5px'
   commentIconElem.style.borderRadius = '5px'
   commentIconElem.style.border = 'solid 1px #ccc'
-  commentIconElem.style.display = 'none'
-  commentIconElem.style.position = 'absolute'
-  commentIconElem.style.top = '0'
-  commentIconElem.style.right = '0'
 
+  const commentCountElem = document.createElement('span')
+  const commentCount = await getCountOfAttestations(sectionId)
+  console.log(commentCount)
+  commentCountElem.textContent = (commentCount).toString()
+  commentCountElem.style.position = 'absolute'
+  commentCountElem.style.top = '-10px'
+  commentCountElem.style.right = '-10px'
+  commentCountElem.style.backgroundColor = '#f00'
+  commentCountElem.style.color = '#fff'
+  commentCountElem.style.borderRadius = '50%'
+  commentCountElem.style.padding = '2px 6px'
+  commentCountElem.style.fontSize = '12px'
+
+  const countAttestation = document.createElement('p')
+  countAttestation.textContent = sectionId.toString()
+  commentIconElem.appendChild(countAttestation)
   return commentIconElem
 }
--- src/queries/CommentIdsQuery.ts
+++ src/queries/CommentIdsQuery.ts
@@ -1,18 +1,9 @@
 import { graphql } from './gql'
 
 export const CommentIdsQuery = graphql(`
-  query CommentIds(
-    $limit: Int
-    $skip: Int = 0
-    $filter: Comment_filter
-  ) {
-    comments(
-      where: $filter
-      first: $limit
-      skip: $skip
-    ) {
-        id
-      }
+  query CommentIds($limit: Int, $skip: Int = 0, $filter: Comment_filter) {
+    comments(where: $filter, first: $limit, skip: $skip) {
+      id
     }
   }
 `)
