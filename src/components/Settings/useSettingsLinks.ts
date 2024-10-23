02b1e10dec31e98728ce2a6d8eb685390f9d5ba0
b4dPot4to <112569797+b4dPot4to@users.noreply.github.com>
2024-10-23 14:00:02 +0200

    add useSettingsLinks hook

---
+++ src/components/Settings/useSettingsLinks.ts
@@ -0,0 +1,46 @@
+import { useTranslation } from 'react-i18next'
+import { SettingView } from './enums'
+
+const useSettingsText = () => {
+  const { t } = useTranslation('nft', { keyPrefix: 'settings' })
+
+  return {
+    generalSettings: {
+      title: t('navigation.general.title'),
+      links: [
+        {
+          label: t('navigation.general.links.general.label'),
+          link: SettingView.GENERAL,
+          icon: 'settings',
+        },
+      ],
+    },
+    accessControl: {
+      title: t('navigation.accessControl.title'),
+      links: [
+        {
+          label: t('navigation.accessControl.links.roles.label'),
+          link: SettingView.ROLES,
+          icon: 'roles',
+        },
+      ],
+    },
+    layoutPreferences: {
+      title: t('navigation.layoutPreferences.title'),
+      links: [
+        {
+          label: t('navigation.layoutPreferences.links.contentEditor.label'),
+          link: SettingView.CONTENT,
+          icon: 'contentEditor',
+        },
+        {
+          label: t('navigation.layoutPreferences.links.layout.label'),
+          link: SettingView.LAYOUT,
+          icon: 'layout',
+        },
+      ],
+    },
+  }
+}
+
+export default useSettingsText
