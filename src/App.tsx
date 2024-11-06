86da6552f2031a31fa4541598cbc1388292ca9b8
Zhuk Evheny <113713601+Beatles93@users.noreply.github.com>
2024-11-06 17:46:05 +0200

    fix: third web

--- src/App.tsx
+++ src/App.tsx
@@ -25,54 +25,59 @@ import { GlobalStyle } from './globalStyle'
 import ConnectWalletPage from './pages/ConnectWalletPage'
 import WalletConnectedProtect from './components/common/WalletConnectedProtect'
 import { HeaderColorProvider } from './components/Nft/NftView/HeaderColorContext'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <ThirdwebProvider
-        supportedChains={supportedChains}
-        activeChain={arbitrumSepoliaChainConfig.chainId}
-        clientId={environment.thirdWebClientId}
-        locale={en()}
-        supportedWallets={[
-          metamaskWallet(),
-          coinbaseWallet({ recommended: true }),
-          walletConnect(),
-        ]}
-      >
-        <ThemeProvider theme={theme}>
-          <HeaderColorProvider>
-            <Router>
-              <Routes>
-                <Route element={<Layout />}>
-                  <Route path={RoutePaths.NFT} element={<NftPage />} />
+    <QueryClientProvider client={queryClient}>
+      <ApolloProvider client={client}>
+        <ThirdwebProvider
+          supportedChains={supportedChains}
+          activeChain={arbitrumSepoliaChainConfig.chainId}
+          clientId={environment.thirdWebClientId}
+          locale={en()}
+          supportedWallets={[
+            metamaskWallet(),
+            coinbaseWallet({ recommended: true }),
+            walletConnect(),
+          ]}
+        >
+          <ThemeProvider theme={theme}>
+            <HeaderColorProvider>
+              <Router>
+                <Routes>
+                  <Route element={<Layout />}>
+                    <Route path={RoutePaths.NFT} element={<NftPage />} />
+                    <Route
+                      path={RoutePaths.NFT + RoutePaths.TOKEN}
+                      element={<TokenPage />}
+                    />
+                    <Route
+                      path={RoutePaths.MY_NFTS}
+                      element={
+                        <WalletConnectedProtect>
+                          <MyNftsPage />
+                        </WalletConnectedProtect>
+                      }
+                    />
+                  </Route>
                   <Route
-                    path={RoutePaths.NFT + RoutePaths.TOKEN}
-                    element={<TokenPage />}
-                  />
-                  <Route
-                    path={RoutePaths.MY_NFTS}
-                    element={
-                      <WalletConnectedProtect>
-                        <MyNftsPage />
-                      </WalletConnectedProtect>
-                    }
-                  />
-                </Route>
-                <Route
-                  path={RoutePaths.CONNECT_WALLET}
-                  element={<ConnectWalletPage />}
-                ></Route>
-              </Routes>
-            </Router>
-            <GlobalStyle />
-            <ToastContainer />
-            <div id='drawers' />
-            <div id='modals' />
-          </HeaderColorProvider>
-        </ThemeProvider>
-      </ThirdwebProvider>
-    </ApolloProvider>
+                    path={RoutePaths.CONNECT_WALLET}
+                    element={<ConnectWalletPage />}
+                  ></Route>
+                </Routes>
+              </Router>
+              <GlobalStyle />
+              <ToastContainer />
+              <div id='drawers' />
+              <div id='modals' />
+            </HeaderColorProvider>
+          </ThemeProvider>
+        </ThirdwebProvider>
+      </ApolloProvider>
+    </QueryClientProvider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
 import './i18n'
 import './index.css'
 import 'react-toastify/dist/ReactToastify.css'
 
-export const queryClient = new QueryClient()
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 )
