a6d16c950fdc3167e1546b3b5ce70157c3914f9a
Zhuk Evheny <113713601+Beatles93@users.noreply.github.com>
2024-11-06 14:15:09 +0200

    style: updated the background in the Roles component

--- src/components/ui/Table/index.tsx
+++ src/components/ui/Table/index.tsx
@@ -1,10 +1,19 @@
-import shouldForwardProp from '@styled-system/should-forward-prop';
-import styled from 'styled-components';
-import { ColorProps, LayoutProps, SpaceProps, TypographyProps, color, layout, space, typography } from 'styled-system';
+import shouldForwardProp from '@styled-system/should-forward-prop'
+import styled from 'styled-components'
+import {
+  ColorProps,
+  LayoutProps,
+  SpaceProps,
+  TypographyProps,
+  color,
+  layout,
+  space,
+  typography,
+} from 'styled-system'
 
 // Extend styled-system props to use with styled-components
-type TableProps = SpaceProps & LayoutProps;
-type TableCellProps = TypographyProps & SpaceProps & ColorProps;
+type TableProps = SpaceProps & LayoutProps
+type TableCellProps = TypographyProps & SpaceProps & ColorProps
 
 // Table container that applies spacing and layout styles from styled-system
 export const Table = styled.table.withConfig({ shouldForwardProp })<TableProps>`
@@ -13,17 +22,19 @@ export const Table = styled.table.withConfig({ shouldForwardProp })<TableProps>`
   width: 100%;
   border-collapse: collapse;
   border: 1px solid #e1e1e1;
-`;
+`
 
 // TableRow with hover effect
 export const TableRow = styled.tr`
   &:hover {
-    background-color: #f5f5f5;
+    background: ${({ theme }) => theme.palette.blueLight};
   }
-`;
+`
 
 // TableHeader with improved typography and spacing
-export const TableHeader = styled.th.withConfig({ shouldForwardProp })<TableCellProps>`
+export const TableHeader = styled.th.withConfig({
+  shouldForwardProp,
+})<TableCellProps>`
   ${typography}
   ${space}
   ${color}
@@ -31,13 +42,15 @@ export const TableHeader = styled.th.withConfig({ shouldForwardProp })<TableCell
   background-color: #fafafa;
   border-bottom: 2px solid #e1e1e1;
   padding: 12px 15px;
-`;
+`
 
 // TableCell with consistent styling and padding
-export const TableCell = styled.td.withConfig({ shouldForwardProp })<TableCellProps>`
+export const TableCell = styled.td.withConfig({
+  shouldForwardProp,
+})<TableCellProps>`
   ${typography}
   ${space}
   ${color}
   padding: 12px 15px;
   border-bottom: 1px solid #e1e1e1;
-`;
+`
