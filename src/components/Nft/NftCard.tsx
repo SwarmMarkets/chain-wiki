4e391f749feaf1199930c70157968836f2a044e4
Zhuk Evheny <113713601+Beatles93@users.noreply.github.com>
2024-11-07 13:58:20 +0200

    sstyle: remove ExplorerLink in the NftCard component and add the Icon

--- src/components/Nft/NftCard.tsx
+++ src/components/Nft/NftCard.tsx
@@ -1,10 +1,9 @@
 import { NFTsQueryFullData } from '@src/shared/utils/ipfs/types'
 import { isSameEthereumAddress } from '@src/shared/utils'
-import { shortenAddress, useAddress } from '@thirdweb-dev/react'
-import React, { useMemo } from 'react'
+import { useAddress, useChainId } from '@thirdweb-dev/react'
+import React, { useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import styled, { useTheme } from 'styled-components'
-import ExplorerLink from '../common/ExplorerLink'
 import RequirePermissions from '../common/RequirePermissions'
 import UploadFileButton from '../common/UploadFileButton'
 import Flex from '../ui/Flex'
@@ -12,6 +11,7 @@ import Icon from '../ui/Icon'
 import Text from '../ui/Text'
 import { StyledCard, Title } from './styled-components'
 import useNFTUpdate from '@src/hooks/useNFTUpdate'
+import { getExplorerUrl } from '@src/shared/utils'
 
 interface NftCardProps {
   nft: NFTsQueryFullData
@@ -23,11 +23,21 @@ const Logo = styled.img`
   max-height: 70px;
 `
 
+const IconContainer = styled.div`
+  position: absolute;
+  top: 40px;
+  right: 25px;
+`
+
 const NftCard: React.FC<NftCardProps> = ({ nft, showRole = false }) => {
   const { t } = useTranslation(['nft', 'nfts'])
   const theme = useTheme()
   const account = useAddress()
   const { signTransaction, tx } = useNFTUpdate(nft.id)
+  const [isHovered, setIsHovered] = useState(false)
+
+  const chainId = useChainId()
+
   const roles = useMemo(() => {
     if (!showRole) return
     const isAdmin = nft.admins.some(address =>
@@ -43,7 +53,6 @@ const NftCard: React.FC<NftCardProps> = ({ nft, showRole = false }) => {
     if (isEditor) {
       roles.push(t('filter.editor', { ns: 'nfts' }))
     }
-
     return roles
   }, [account, nft.admins, nft.editors, t, showRole])
 
@@ -51,8 +60,17 @@ const NftCard: React.FC<NftCardProps> = ({ nft, showRole = false }) => {
     await signTransaction({ logoUrl: url })
   }
 
+  const handleIconClick = () => {
+    const explorerUrl = getExplorerUrl({
+      type: 'address',
+      chainId,
+      hash: nft.id,
+    })
+    window.open(explorerUrl, '_blank')
+  }
+
   return (
-    <StyledCard minHeight={200}>
+    <StyledCard minHeight={200} position='relative'>
       <Flex flexDirection='column' justifyContent='space-between' height='100%'>
         <Flex alignItems='center' $gap='3px'>
           <Icon cursor='pointer' name='document' size={40} />
@@ -74,20 +92,23 @@ const NftCard: React.FC<NftCardProps> = ({ nft, showRole = false }) => {
           )}
         </Flex>
         <Flex flexDirection='column' alignItems='end' pt={10} $gap='2px'>
-          <ExplorerLink iconSize={10} type='address' hash={nft.id}>
-            <Text
-              fontSize={theme.fontSizes.small}
-              color={theme.palette.linkPrimary}
-            >
-              {shortenAddress(nft.id, false)}
-            </Text>
-          </ExplorerLink>
           {roles && roles.length > 0 && (
             <Text fontSize={theme.fontSizes.small}>
               {t('roles', { ns: 'nfts' })} {roles.join(', ')}
             </Text>
           )}
         </Flex>
+        <IconContainer>
+          <Icon
+            cursor='pointer'
+            name='externalLink'
+            size={10}
+            color={isHovered ? theme.palette.linkPrimary : theme.palette.black}
+            onClick={handleIconClick}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          />
+        </IconContainer>
       </Flex>
     </StyledCard>
   )
