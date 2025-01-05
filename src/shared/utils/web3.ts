eb8852d2335da0674ac13b7ce3d5e3659f17837b
arsen-lulukian <arsen@swarm.com>
2025-01-05 18:23:58 +0200

    feat: add utils to resolve thirdweb transactions

--- src/shared/utils/web3.ts
+++ src/shared/utils/web3.ts
@@ -43,11 +43,47 @@ export const checkNetworkSupported = (chainId?: number) => {
 // Function to convert a string to a byte array and then hash it using keccak256
 export const stringToByteArray = (str: string) => {
   // Convert the string to a byte array
-  const bytes = ethers.utils.toUtf8Bytes(str);
+  const bytes = ethers.utils.toUtf8Bytes(str)
 
   // Hash the byte array using keccak256
-  const hashed = ethers.utils.keccak256(bytes);
+  const hashed = ethers.utils.keccak256(bytes)
 
   // Return the hashed value
-  return hashed;
+  return hashed
+}
+
+import { TransactionBase } from '@safe-global/types-kit'
+import { Transaction } from '@thirdweb-dev/sdk'
+
+export const resolveThirdwebTransaction = async (
+  thirdwebTx?: Transaction
+): Promise<TransactionBase | null> => {
+  if (!thirdwebTx) return null
+  const populatedTx = await thirdwebTx.populateTransaction()
+
+  const to = populatedTx.to
+  const value = populatedTx.value?.toString() ?? ''
+  const data = populatedTx.data?.toString()
+
+  if (to && data) {
+    return {
+      to,
+      value,
+      data,
+    }
+  }
+
+  return null
+}
+
+export const resolveAllThirdwebTransactions = async (
+  thirdwebTxs?: Transaction[]
+): Promise<TransactionBase[]> => {
+  if (!thirdwebTxs) return []
+  const resolvedTxs = await Promise.all(
+    thirdwebTxs.map(tx => resolveThirdwebTransaction(tx))
+  )
+  return resolvedTxs.filter(
+    (tx): tx is TransactionBase => tx !== null && tx !== undefined
+  )
 }
