339915bcc9cab72e77dbd96a18829fba0c977b53
arsen-lulukian <arsen@swarm.com>
2025-02-20 19:32:32 +0200

    feat: update chainwiki badge

--- src/globalStyle.ts
+++ src/globalStyle.ts
@@ -3,7 +3,7 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
   body {
     /* background: ${({ theme }) => theme.palette.bgPrimary}; */
-    color: ${({ theme }) => theme.palette.textPrimary};
+    /* color: ${({ theme }) => theme.palette.textPrimary}; */
     font-size: ${({ theme }) => theme.fontSizes.medium};
     font-family: ${({ theme }) => theme.fontFamilies.roboto};
   }
