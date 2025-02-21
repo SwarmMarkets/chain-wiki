42ecaec2e3ecdc9b7f4f7a275d2a4232ef5b83f0
arsen-lulukian <arsen@swarm.com>
2025-02-21 10:25:13 +0200

    fix: sidebar scroll

--- src/components/common/Layout/NftLayout.tsx
+++ src/components/common/Layout/NftLayout.tsx
@@ -95,15 +95,15 @@ const NftLayout = () => {
         </Link>
       </header>
 
-      <div className='flex flex-1'>
-        <aside className='w-64 bg-paper flex flex-col h-full border-r-gray-200 border-r'>
+      <div className='flex flex-1 min-h-0'>
+        <aside className='w-64 bg-paper flex flex-col border-r-gray-200 border-r overflow-y-auto h-full'>
           <nav className='flex-1 overflow-y-auto p-4 flex flex-col gap-1'>
             <IndexPages tokens={fullTokens} nft={nft} />
           </nav>
-          <footer className='p-4'></footer>
+          <footer></footer>
         </aside>
 
-        <div className='flex flex-col flex-1'>
+        <div className='flex flex-col flex-1 min-h-0'>
           <div className='flex-1 overflow-auto p-4'>
             <Outlet />
           </div>
