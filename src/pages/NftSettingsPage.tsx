32bacf15f874d87e982448c73300acb4f1961f94
arsen-lulukian <arsen@swarm.com>
2025-03-14 14:22:16 +0200

    Update NftSettingsPage.tsx

--- src/pages/NftSettingsPage.tsx
+++ src/pages/NftSettingsPage.tsx
@@ -1,10 +1,16 @@
 import Settings from 'src/components/Settings/Settings'
+import Fade from 'src/components/ui-kit/Animations/Fade'
+import { AnimatePresence } from 'framer-motion'
 
 const NftSettingsPage = () => {
   return (
-    <div>
-      <Settings />
-    </div>
+    <AnimatePresence>
+      <Fade>
+        <div>
+          <Settings />
+        </div>
+      </Fade>
+    </AnimatePresence>
   )
 }
 export default NftSettingsPage
