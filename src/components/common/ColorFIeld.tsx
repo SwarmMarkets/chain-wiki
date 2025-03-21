dd25c4539163ae2b54b3a1ee503bd6397b165778
arsen-lulukian <arsen@swarm.com>
2025-03-21 11:11:23 +0200

    Update ColorFIeld.tsx

--- src/components/common/ColorFIeld.tsx
+++ src/components/common/ColorFIeld.tsx
@@ -20,7 +20,7 @@ const ColorField: React.FC<ColorFieldProps> = ({
     <div className={clsx('flex items-center', className)}>
       <TextField value={color} onChange={onChange} className='w-24' />
       <div
-        className='w-6 h-6 ml-2 border rounded cursor-pointer relative'
+        className='w-6 h-6 ml-2 border border-main rounded cursor-pointer relative'
         style={{ backgroundColor: color }}
         onClick={() => colorPicker.toggle()}
       >
