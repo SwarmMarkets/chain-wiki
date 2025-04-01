ce386e2a3f86cd54a32fe53c0518cfbf9e03f9d8
Zhuk Evheny <113713601+Beatles93@users.noreply.github.com>
2025-04-01 12:42:16 +0200

    feat: added skeleton to NftPage

--- src/components/Nft/NftContentSkeleton.tsx
+++ src/components/Nft/NftContentSkeleton.tsx
@@ -1,31 +1,31 @@
-import Skeleton from '../ui-kit/Skeleton/Skeleton'
+import Skeleton from 'src/components/ui-kit/Skeleton/Skeleton'
 
 const NftContentSkeleton = () => {
   return (
     <div className='w-full flex flex-col gap-4'>
       <Skeleton
-        width='50%'
+        width='100%'
+        height='3rem'
+        className='ui-skeleton ui-skeleton-rect'
+      />
+      <Skeleton
+        width='100'
         height='2rem'
         className='ui-skeleton ui-skeleton-rect'
       />
       <Skeleton
-        width='80%'
-        height='1rem'
+        width='100%'
+        height='2rem'
         className='ui-skeleton ui-skeleton-rect'
       />
       <Skeleton
-        width='60%'
-        height='1rem'
+        width='100%'
+        height='2rem'
         className='ui-skeleton ui-skeleton-rect'
       />
       <Skeleton
-        width='90%'
-        height='1rem'
-        className='ui-skeleton ui-skeleton-rect'
-      />
-      <Skeleton
-        width='70%'
-        height='1rem'
+        width='100%'
+        height='50rem'
         className='ui-skeleton ui-skeleton-rect'
       />
     </div>
