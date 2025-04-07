618cc81cb568ebbf78e2e07441a16cffa7ff90fc
Zhuk Evheny <113713601+Beatles93@users.noreply.github.com>
2025-04-07 16:41:44 +0200

    fix: Replace inline styles with styled-components in GeneralSettings component

--- src/components/Nft/NftView/GeneralSettings.tsx
+++ src/components/Nft/NftView/GeneralSettings.tsx
@@ -1,13 +1,30 @@
 import React, { useState } from 'react'
 import TextField from 'src/components/ui-kit/TextField/TextField'
-import Flex from 'src/components/ui/Flex'
 import UpdateNftContentButton from 'src/components/UpdateContent/UpdateNftContentButton'
 import useNFT from 'src/hooks/subgraph/useNFT'
 import { useTranslation } from 'react-i18next'
+import styled from 'styled-components'
 
 interface GeneralSettingsProps {
   nftAddress: string
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  gap: 8px;
+  align-items: center;
+`
+
+const MarginBottom = styled.div`
+  margin-bottom: 12px;
+`
 
 const GeneralSettings: React.FC<GeneralSettingsProps> = ({ nftAddress }) => {
   const { nft } = useNFT(nftAddress, { disableRefetch: true })
@@ -21,24 +38,24 @@ const GeneralSettings: React.FC<GeneralSettingsProps> = ({ nftAddress }) => {
   const nameValue = name === null ? nft?.name : name
 
   return (
-    <Flex flexDirection='column' $gap='16px'>
-      <Flex alignItems='center' $gap='8px'>
+    <Container>
+      <GridContainer>
         <TextField
           value={nameValue}
           onChange={handleNameChange}
           inputProps={{ placeholder: 'Enter site name' }}
-          style={{ flexGrow: 1 }}
         />
-        <UpdateNftContentButton
-          nftAddress={nftAddress}
-          nftContentToUpdate={{ name: nameValue }}
-          disabled={!nameValue || nameValue === nft?.name}
-          style={{ marginBottom: '12px' }} 
-        >
-          {t('save')}
-        </UpdateNftContentButton>
-      </Flex>
-    </Flex>
+        <MarginBottom>
+          <UpdateNftContentButton
+            nftAddress={nftAddress}
+            nftContentToUpdate={{ name: nameValue }}
+            disabled={!nameValue || nameValue === nft?.name}
+          >
+            {t('save')}
+          </UpdateNftContentButton>
+        </MarginBottom>
+      </GridContainer>
+    </Container>
   )
 }
 
