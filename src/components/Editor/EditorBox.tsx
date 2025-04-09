b4950512a41378ff691c5439aa80f7099c804fff
arsen-lulukian <arsen@swarm.com>
2025-04-09 14:45:59 +0300

    feat: mdx editor

--- src/components/Editor/EditorBox.tsx
+++ src/components/Editor/EditorBox.tsx
@@ -2,13 +2,33 @@ import { Editor as TinyEditor } from '@tinymce/tinymce-react'
 import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
 import React, { useRef } from 'react'
 import { storage } from 'src/firebase'
+import {
+  headingsPlugin,
+  listsPlugin,
+  quotePlugin,
+  thematicBreakPlugin,
+  toolbarPlugin,
+  markdownShortcutPlugin,
+  linkPlugin,
+  tablePlugin,
+  codeBlockPlugin,
+  MDXEditor,
+  UndoRedo,
+  BoldItalicUnderlineToggles,
+  MDXEditorMethods,
+  ListsToggle,
+  InsertImage,
+  BlockTypeSelect,
+  imagePlugin,
+} from '@mdxeditor/editor'
 
-import './editor.css'
+import '@mdxeditor/editor/style.css'
+import { BulletList } from 'react-content-loader'
 
 interface EditorBoxProps {
   initialContent?: string
   content?: string
-  onChange: (content: string, editor: TinyEditor) => void
+  onChange: (content: string) => void
   onEditorInit?: (editorInit: boolean) => void
 }
 
@@ -20,8 +40,8 @@ const EditorBox: React.FC<EditorBoxProps> = ({
 }) => {
   const editorRef = useRef<TinyEditor | null>(null)
 
-  const onEditorChange = (content: string, editor: TinyEditor) => {
-    onChange && onChange(content, editor)
+  const onEditorChange = (content: string) => {
+    onChange && onChange(content)
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -48,39 +68,37 @@ const EditorBox: React.FC<EditorBoxProps> = ({
     onEditorInit?.(true)
   }
 
-  console.log(content)
+  const mdxRef = useRef<MDXEditorMethods>(null)
 
   return (
-    <TinyEditor
-      // tinymceScriptSrc='https://cdnjs.cloudflare.com/ajax/libs/tinymce/7.6.0/tinymce.min.js'
-      ref={editorRef}
-      apiKey='eq9n6yyh5256xn3u2dihjv9n2cjd2g4tb15cwa50xidbrmgh'
-      onEditorChange={onEditorChange}
-      onInit={onInitEdiror}
-      value={content}
-      init={{
-        plugins:
-          'markdown anchor autolink charmap codesample emoticons image link lists searchreplace table visualblocks wordcount',
-        toolbar:
-          'undo redo | blocks fontsize | bold italic underline strikethrough | link image media table mergetags | align | tinycomments | numlist bullist indent outdent | emoticons charmap | removeformat',
-        tinycomments_mode: 'embedded',
-        tinycomments_author: 'Author name',
-        mergetags_list: [
-          { value: 'First.Name', title: 'First Name' },
-          { value: 'Email', title: 'Email' },
-        ],
-        height: 650,
-        font_size_formats: '10px 12px 14px 16px 18px 24px 36px 48px',
-        menubar: false,
-        image_description: true, // Включаем поле описания для изображений
-        image_caption: true, // Включаем подписи для изображений
-        // automatic_uploads: true,
-        // images_reuse_filename: true,
-        images_upload_handler: handleImageUpload,
-        content_style:
-          'body { font-family: "Inter", sans-serif; font-size: 14px; }',
-      }}
-      initialValue={initialContent}
+    <MDXEditor
+      className='w-full'
+      contentEditableClassName='prose prose-sm md:prose-base lg:prose-lg'
+      ref={mdxRef}
+      markdown={content || ''}
+      onChange={onEditorChange}
+      plugins={[
+        headingsPlugin(),
+        listsPlugin(),
+        quotePlugin(),
+        thematicBreakPlugin(),
+        toolbarPlugin({
+          toolbarContents: () => (
+            <>
+              <UndoRedo />
+              <BoldItalicUnderlineToggles />
+              <BlockTypeSelect />
+              <ListsToggle options={['bullet', 'number']} />
+              {/* <InsertImage /> */}
+            </>
+          ),
+        }),
+        markdownShortcutPlugin(),
+        linkPlugin(),
+        tablePlugin(),
+        codeBlockPlugin(),
+        // imagePlugin({ imageUploadHandler: handleImageUpload }),
+      ]}
     />
   )
 }
