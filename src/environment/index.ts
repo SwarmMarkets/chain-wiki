d48396c3179a9076cc7c2d6d616ad23505c7a6b6
arsen-lulukian <arsen@swarm.com>
2025-04-22 18:56:56 +0300

    feat: add isDev and isProd env vars

--- src/environment/index.ts
+++ src/environment/index.ts
@@ -1,5 +1,9 @@
+const isDev = process.env.REACT_APP_MODE === 'development'
+const isProd = process.env.REACT_APP_MODE === 'production'
 
 export const environment = Object.freeze({
+  isDevMode: isDev,
+  isProdMode: isProd,
   thirdWebClientId: process.env.REACT_APP_THIRD_WEB_CLIENT_ID,
   subgraphApiKey: process.env.REACT_APP_SUBGRAPH_API_KEY,
 })
