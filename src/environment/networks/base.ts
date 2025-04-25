59380c9ee00910a343b7a98c4c6fb950b6430e58
arsen-lulukian <arsen@swarm.com>
2025-04-25 15:27:34 +0300

    feat: update test subgraph url to prod for base

--- src/environment/networks/base.ts
+++ src/environment/networks/base.ts
@@ -1,8 +1,9 @@
 import { NetworkConfiguration } from 'src/shared/types/network-configuration'
 import { Base, Chain } from '@thirdweb-dev/chains'
+import { environment } from '..'
 
 export const baseEnvironment: NetworkConfiguration = Object.freeze({
-  subgraphURL: `https://api.studio.thegraph.com/query/60829/chainwiki-base/version/latest`,
+  subgraphURL: `https://gateway.thegraph.com/api/${environment.subgraphApiKey}/subgraphs/id/3PWCiLW4ZxG3o6RrJBgUaaYKkeMNuUsSVsHbmE4ptufq`,
   contracts: {
     sx1155NFTFactoryAddress: '0x04620054fB4052Faf2a6A3F664149d574A907193',
   },
