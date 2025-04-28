0363ff1c4d278a99522439cfc83907f61f919e54
Zhuk Evheny <113713601+Beatles93@users.noreply.github.com>
2025-04-28 15:58:56 +0200

    fix: added skeleton for MetaMask, removed bg-primary background, and set gray-200

--- src/components/common/Layout/ReadLayout/ReadHeaderSkeleton.tsx
+++ src/components/common/Layout/ReadLayout/ReadHeaderSkeleton.tsx
@@ -2,14 +2,14 @@ import Skeleton from 'src/components/ui-kit/Skeleton/Skeleton'
 
 const ReadHeaderSkeleton = () => {
   return (
-    <header className='bg-primary py-3 w-full'>
+    <header className='gray-200 py-3 w-full'>
       <div className='max-w-screen-2xl mx-auto px-4 sm:px-6 md:px-8 flex items-center max-h-10 justify-between'>
         <Skeleton
           width='100px'
           height='3rem'
-          className='ui-skeleton ui-skeleton-rect'
+          className='ui-skeleton ui-skeleton-rect self-center'
         />
-        <div className='flex gap-6'>
+        <div className='flex gap-8 items-center'>
           <Skeleton
             width='80px'
             height='1.5rem'
@@ -26,34 +26,9 @@ const ReadHeaderSkeleton = () => {
             className='ui-skeleton ui-skeleton-rect'
           />
           <Skeleton
-            width='80px'
-            height='1.5rem'
-            className='ui-skeleton ui-skeleton-rect'
-          />
-          <Skeleton
-            width='80px'
-            height='1.5rem'
-            className='ui-skeleton ui-skeleton-rect'
-          />
-          <Skeleton
-            width='80px'
-            height='1.5rem'
-            className='ui-skeleton ui-skeleton-rect'
-          />
-          <Skeleton
-            width='80px'
-            height='1.5rem'
-            className='ui-skeleton ui-skeleton-rect'
-          />
-          <Skeleton
-            width='80px'
-            height='1.5rem'
-            className='ui-skeleton ui-skeleton-rect'
-          />
-          <Skeleton
-            width='80px'
-            height='1.5rem'
-            className='ui-skeleton ui-skeleton-rect'
+            width='180px'
+            height='3.5rem'
+            className='ui-skeleton ui-skeleton-rounded self-center'
           />
         </div>
       </div>
