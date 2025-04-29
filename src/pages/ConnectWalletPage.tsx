92db296fbc680d82b1f3046371d649c045897f21
arsen-lulukian <arsen@swarm.com>
2025-04-29 09:41:43 +0300

    feat: update connect wallet card style

--- src/pages/ConnectWalletPage.tsx
+++ src/pages/ConnectWalletPage.tsx
@@ -1,5 +1,4 @@
 import ConnectButton from 'src/components/common/ConnectButton'
-import Card from 'src/components/ui/Card'
 import { useConnectionStatus } from '@thirdweb-dev/react'
 import { useTranslation } from 'react-i18next'
 import { Navigate, useNavigate } from 'react-router-dom'
@@ -19,13 +18,11 @@ const ConnectWalletPage = () => {
 
   return (
     <div className='h-screen flex justify-center items-center bg-gradient-to-br from-[#c2ebfb] to-[#a1a7fd]'>
-      <Card p='40px'>
-        <div className='flex flex-col items-center'>
-          <img src={'assets/logo.png'} alt='ChainWiki' className='w-[230px]' />
-          <h1 className='typo-title3 mt-5 mb-7'>{t('title')}</h1>
-          <ConnectButton onConnect={handleConnectWallet} />
-        </div>
-      </Card>
+      <div className='flex flex-col items-center bg-paper py-12 px-16 rounded-2xl shadow-xl'>
+        <img src={'assets/logo.png'} alt='ChainWiki' className='w-72' />
+        <h1 className='typo-title2 font-medium mt-5 mb-6'>{t('title')}</h1>
+        <ConnectButton onConnect={handleConnectWallet} />
+      </div>
     </div>
   )
 }
