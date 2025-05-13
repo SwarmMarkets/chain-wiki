d593d033acc82106ef8bbdea9d7c003e9124aa8c
Zhuk Evheny <113713601+Beatles93@users.noreply.github.com>
2025-05-13 12:56:39 +0200

    feat: add types for toast notifications

---
+++ src/shared/types/toast-types.ts
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react'
+
+export type ToastType = 'info' | 'success' | 'warn' | 'error'
+
+export interface Toast {
+  id: number
+  message: ReactNode
+  type?: ToastType
+  actionHref?: string
+  actionText?: string
+}
