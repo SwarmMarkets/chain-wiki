8ea16241205aa9e026388ed53050541ea61f3233
arsen-lulukian <arsen@swarm.com>
2025-06-09 15:46:45 +0300

    feat: update description

--- src/components/Settings/SettingsBody/IntegrationSetting/index.tsx
+++ src/components/Settings/SettingsBody/IntegrationSetting/index.tsx
@@ -12,7 +12,23 @@ const IntegrationSetting = () => {
       description={
         <div>
           <div>{t('import.description')}</div>
-          <div className='mt-1.5'>{t('import.descriptionNote')}</div>
+          <div className='mt-1.5'>
+            <div>{t('import.instructions.title')}</div>
+            <ol className='list-decimal list-inside mt-2 space-y-1'>
+              <li>
+                {t('import.instructions.steps.sync')}
+                <a
+                  href='https://gitbook.com/docs/getting-started/git-sync/enabling-github-sync'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='text-blue-500 hover:text-primary'
+                >
+                  https://gitbook.com/docs/getting-started/git-sync/enabling-github-sync
+                </a>
+              </li>
+              <li>{t('import.instructions.steps.public')}</li>
+            </ol>
+          </div>
         </div>
       }
     >
