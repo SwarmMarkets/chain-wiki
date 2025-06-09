5931bd31edbf0276834b1c2834172e57f8adfe31
arsen-lulukian <arsen@swarm.com>
2025-06-09 12:25:37 +0300

    fix: contract call to update NFT

--- src/hooks/useNFTUpdate.ts
+++ src/hooks/useNFTUpdate.ts
@@ -88,7 +88,7 @@ const useNFTUpdate = (nftAddress: string) => {
     (nftContentToUpdate: NFTContentToUpdate) => {
       const nftUpdateJson = JSON.stringify(nftContentToUpdate)
 
-      return call('setTokenKya', [nftUpdateJson])
+      return call('setContractKya', [nftUpdateJson])
     },
     [call]
   )
