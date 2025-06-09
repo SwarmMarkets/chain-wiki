1a60ddcc7888f67397eb8648ab7c8b7e50430463
arsen-lulukian <arsen@swarm.com>
2025-06-09 12:04:51 +0300

    feat: update explore page

--- src/pages/ExplorePage.tsx
+++ src/pages/ExplorePage.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from 'react-i18next'
-import { generatePath } from 'react-router-dom'
+import { generatePath, Link } from 'react-router-dom'
 import NftList from 'src/components/Nft/NftList'
+import Button from 'src/components/ui-kit/Button/Button'
 import useNFTs from 'src/hooks/subgraph/useNFTs'
 import { Nft_OrderBy, OrderDirection } from 'src/queries/gql/graphql'
 import RoutePaths from 'src/shared/enums/routes-paths'
@@ -19,31 +20,42 @@ const ExplorePage = () => {
   const noNfts = !loading && (!nfts || nfts?.length === 0)
 
   return (
-    <div className='h-full'>
-      <div
-        className='px-20 py-10'
-        style={{
-          background: 'linear-gradient(60deg,#c2ebfb 0%, #a1a7fd 100%)',
-        }}
-      >
-        <img className='max-w-96' src='assets/logo.png' alt='ChainWiki' />
-      </div>
-      <div className='px-20 mt-7'>
-        <h1 className='typo-heading1 text-main-accent font-medium'>
-          {t('title')}
-        </h1>
-        <h3 className='heading-md'>{t('subtitle')}</h3>
-        {noNfts ? (
-          <div className='text-center mt-14 typo-title2'>{t('noNfts')}</div>
-        ) : (
-          <NftList
-            loading={loading}
-            nfts={nfts}
-            skeletonLength={10}
-            className='mt-7'
-            to={nft => generatePath(RoutePaths.NFT_READ, { nftId: nft.id })}
-          />
-        )}
+    <div
+      className='h-screen flex flex-col items-center'
+      style={{ background: 'rgb(249, 249, 249)' }}
+    >
+      <div className='w-full max-w-screen-lg mt-10'>
+        <div
+          className='rounded-full w-full px-5 py-2.5 flex items-center justify-between'
+          style={{ boxShadow: 'rgba(0, 0, 0, 0.25) 0px 0px 10px 0px' }}
+        >
+          <div className='p-2.5'>
+            <img className='max-h-7' src='assets/logo.png' alt='ChainWiki' />
+          </div>
+          <Link to={RoutePaths.HOME}>
+            <Button size='md' style={{ borderRadius: '50px', height: 43 }}>
+              Open App
+            </Button>
+          </Link>
+        </div>
+
+        <div className='px-20 mt-9'>
+          <h1 className='typo-heading1 text-main-accent font-medium'>
+            {t('title')}
+          </h1>
+          <h3 className='heading-md'>{t('subtitle')}</h3>
+          {noNfts ? (
+            <div className='text-center mt-14 typo-title2'>{t('noNfts')}</div>
+          ) : (
+            <NftList
+              loading={loading}
+              nfts={nfts}
+              skeletonLength={10}
+              className='mt-7'
+              to={nft => generatePath(RoutePaths.NFT_READ, { nftId: nft.id })}
+            />
+          )}
+        </div>
       </div>
     </div>
   )
