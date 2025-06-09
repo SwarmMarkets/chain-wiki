8129b37e605feec6289e19820e5f78e8fbe2c4ef
arsen-lulukian <arsen@swarm.com>
2025-06-09 12:21:18 +0300

    Update useNFTUpdate.ts

--- src/hooks/useNFTUpdate.ts
+++ src/hooks/useNFTUpdate.ts
@@ -88,7 +88,7 @@ const useNFTUpdate = (nftAddress: string) => {
     (nftContentToUpdate: NFTContentToUpdate) => {
       const nftUpdateJson = JSON.stringify(nftContentToUpdate)
 
-      return call('setKya', [nftUpdateJson])
+      return call('setTokenKya', [nftUpdateJson])
     },
     [call]
   )
