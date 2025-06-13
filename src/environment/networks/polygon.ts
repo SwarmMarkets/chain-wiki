021238e29076495cd88e7bb340f7755a04eaadbd
b4dPot4to <eduard@swarm.com>
2025-06-13 02:32:10 +0200

    remove temporar env, use dev url

--- src/environment/networks/polygon.ts
+++ src/environment/networks/polygon.ts
@@ -1,9 +1,9 @@
 import { NetworkConfiguration } from 'src/shared/types/network-configuration'
 import { Polygon, Chain } from '@thirdweb-dev/chains'
-import { environment } from '..'
 
 export const polygonEnvironment: NetworkConfiguration = Object.freeze({
-  subgraphURL: environment.polygonSubgraphUrl || '',
+  subgraphURL:
+    'https://gateway.thegraph.com/api/subgraphs/id/HoZZ8DKMpdN5A4axJ2nfrnEzsYfx4jik84iooR2RgoJ3',
   contracts: {
     sx1155NFTFactoryAddress: '0x9bfF9401F1807cDC9DcF48E67869Cf555244cE7C',
   },
