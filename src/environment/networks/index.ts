2be9f04998bcdb1cefd0182fd0635548ce788f37
arsen-lulukian <arsen@swarm.com>
2025-06-24 23:10:06 +0300

    feat: update networks config

--- src/environment/networks/index.ts
+++ src/environment/networks/index.ts
@@ -5,12 +5,12 @@ import {
 import { baseChainConfig, baseEnvironment } from './base'
 import { polygonChainConfig, polygonEnvironment } from './polygon'
 import { environment } from '..'
-import { Chain } from 'thirdweb'
+import { Chain } from 'thirdweb/chains'
 
 export enum SupportedChainId {
-  Base = baseChainConfig.chainId,
-  ArbitrumSepolia = arbitrumSepoliaChainConfig.chainId,
-  Polygon = polygonChainConfig.chainId,
+  Base = baseChainConfig.id,
+  ArbitrumSepolia = arbitrumSepoliaChainConfig.id,
+  Polygon = polygonChainConfig.id,
 }
 
 const polygonConfig = environment.subgraphApiKey ? [polygonChainConfig] : []
--- src/environment/networks/polygon.ts
+++ src/environment/networks/polygon.ts
@@ -1,12 +1,11 @@
 import { NetworkConfiguration } from 'src/shared/types/network-configuration'
-import { Polygon, Chain } from '@thirdweb-dev/chains'
-import { environment } from '..'
+import { Chain, polygon } from 'thirdweb/chains'
 
 export const polygonEnvironment: NetworkConfiguration = Object.freeze({
-  subgraphURL: environment.polygonSubgraphUrl || '',
+  subgraphURL: '',
   contracts: {
     sx1155NFTFactoryAddress: '0x9bfF9401F1807cDC9DcF48E67869Cf555244cE7C',
   },
 })
 
-export const polygonChainConfig: Chain = Polygon
+export const polygonChainConfig: Chain = polygon
