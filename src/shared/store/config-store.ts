4fbd9576ee0cba58d93adb111f94db4ee49bedb9
arsen-lulukian <arsen@swarm.com>
2025-06-24 22:58:00 +0300

    feat: update store

--- src/shared/store/config-store.ts
+++ src/shared/store/config-store.ts
@@ -3,14 +3,14 @@ import { persist } from 'zustand/middleware'
 import staticConfig from 'src/config'
 
 interface ConfigStore {
-  lastChainId: number | null
+  lastChainId: number
   setLastChainId: (chainId: number) => void
 }
 
 export const useConfigStore = create<ConfigStore>()(
   persist(
     set => ({
-      lastChainId: null,
+      lastChainId: staticConfig.defaultChain.id,
       setLastChainId: chainId =>
         set(() => ({
           lastChainId: chainId,
