781a4e35a3a39d50a0476b1c7904f5e3fcf54017
arsen-lulukian <arsen@swarm.com>
2025-06-30 23:51:41 +0300

    fix: upload of generated html to ipfs

--- src/components/Settings/SettingsBody/ENSDomainSetting/SetupENSForm.tsx
+++ src/components/Settings/SettingsBody/ENSDomainSetting/SetupENSForm.tsx
@@ -4,9 +4,7 @@ import TextField from 'src/components/ui-kit/TextField/TextField'
 import { generateSiteLink } from 'src/shared/utils'
 import useSetupENSForm, { SetupENSFormInputs } from './useSetupENSForm'
 import { generateRedirectHtml } from './utils'
-
 import { encode } from '@ensdomains/content-hash'
-import { upload } from 'thirdweb/storage'
 import { ethers } from 'ethers'
 import { namehash } from 'ethers/lib/utils'
 import { useState } from 'react'
@@ -16,13 +14,12 @@ import {
   getENSResolver,
   getENSResolverInterface,
 } from 'src/hooks/contracts/ens/getENSResolver'
-import { useToastManager } from 'src/hooks/useToastManager'
 import useNFTIdParam from 'src/hooks/useNftIdParam'
 import { ethereum } from 'thirdweb/chains'
 import { useSwitchActiveWalletChain } from 'thirdweb/react'
-import { thirdwebClient } from 'src/shared/api-clients/thirdweb'
 import { multicall } from 'src/thirdweb/ens-resolver'
 import { Address } from 'thirdweb'
+import { useIpfsUpload } from 'src/hooks/web3/useIpfsUpload'
 import useSendTx from 'src/hooks/web3/useSendTx'
 
 const { supportedChains } = staticConfig
@@ -38,7 +35,7 @@ const SetupENSForm = () => {
     formState: { errors },
     reset,
   } = useSetupENSForm()
-  const { addToast } = useToastManager()
+  const { mutateAsync: upload } = useIpfsUpload()
   const [submitLoading, setSubmitLoading] = useState(false)
   const { sendTx } = useSendTx()
 
@@ -49,10 +46,7 @@ const SetupENSForm = () => {
 
     const uploadHtmlToIpfs = async (html: string): Promise<string> => {
       const file = new File([html], 'index.html', { type: 'text/html' })
-      const [ipfsUrl] = await upload({
-        files: [file],
-        client: thirdwebClient,
-      })
+      const ipfsUrl = (await upload([file])) as string
       return ipfsUrl
     }
 
@@ -89,18 +83,13 @@ const SetupENSForm = () => {
         contract: resolver,
       })
 
-      await sendTx(multicallTx)
+      await sendTx(multicallTx, {
+        successMessage: t('messages.success'),
+        errorMessage: t('messages.failed'),
+      })
 
       reset()
       switchChain(supportedChains[0])
-      addToast(t('messages.success'), {
-        type: 'success',
-      })
-    } catch (err) {
-      addToast(err.message || t('messages.failed'), {
-        type: 'error',
-      })
-      console.error(err)
     } finally {
       setSubmitLoading(false)
     }
