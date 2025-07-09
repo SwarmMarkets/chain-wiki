66e5dc580644b05058d449f1fde050ca12315262
basya01 <arsen@swarm.com>
2025-07-09 14:46:22 +0300

    fix: ethereum type declaration

--- src/shared/types/global.d.ts
+++ src/shared/types/global.d.ts
@@ -1,14 +1,3 @@
-declare global {
-  interface EthereumProvider {
-    isMetaMask?: boolean
-    request: (args: { method: string; params?: unknown[] }) => Promise<unknown>
-  }
-
-  interface Window {
-    ethereum?: EthereumProvider
-  }
-}
-
 declare module '*.module.css' {
   const classes: { [key: string]: string }
   export default classes
