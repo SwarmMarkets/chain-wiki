643b52752a84d1757bc2359afa8dda691f935306
basya01 <arsen@swarm.com>
2025-09-22 14:12:18 +0300

    feat: add auto chain switching

--- src/hooks/web3/useHandleSwitchChain.ts
+++ src/hooks/web3/useHandleSwitchChain.ts
@@ -1,22 +1,22 @@
-import { useSearchParams } from 'react-router-dom'
+import { useParams, useSearchParams } from 'react-router-dom'
 import {
   useActiveWalletConnectionStatus,
   useSwitchActiveWalletChain,
 } from 'thirdweb/react'
 import useActiveOrDefaultChain from './useActiveOrDefaultChain'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { getChainByName } from 'src/shared/utils'
 import { useConfigStore } from 'src/shared/store/config-store'
 import useEffectCompare from '../useEffectCompare'
 import { baseChainConfig } from 'src/environment/networks/base'
+import { polygonChainConfig } from 'src/environment/networks/polygon'
+import useNftBySlugOnChains from '../subgraph/useNftBySlugOnChains'
 
-// Accept an optional disabled flag to noop in preview-like contexts
 const useHandleSwitchChain = (disabled?: boolean) => {
   const chain = useActiveOrDefaultChain()
   const setLastChainId = useConfigStore(state => state.setLastChainId)
   const [searchParams] = useSearchParams()
   const switchChain = useSwitchActiveWalletChain()
-
   const status = useActiveWalletConnectionStatus()
 
   const chainNameSearchParam = searchParams.get('chain')
@@ -24,26 +24,50 @@ const useHandleSwitchChain = (disabled?: boolean) => {
     ? getChainByName(chainNameSearchParam)
     : baseChainConfig
 
+  const { nftIdOrSlug = '' } = useParams()
+
+  const { baseNft, polygonNft, loading } = useNftBySlugOnChains(nftIdOrSlug)
+
+  const [conflict, setConflict] = useState(false)
+
   useEffect(() => {
-    if (disabled) return
+    if (disabled || loading) return
 
     const handleChainChange = async () => {
-      if (status === 'connected') {
-        await switchChain(chain)
+      if (baseNft && polygonNft) {
+        setConflict(true)
+        return
       }
 
-      if (chainBySearchParam) {
-        setLastChainId(chainBySearchParam.id)
+      let targetChain = chainBySearchParam
+
+      if (baseNft && !polygonNft) {
+        targetChain = baseChainConfig
+      } else if (!baseNft && polygonNft) {
+        targetChain = polygonChainConfig
+      }
+
+      if (status === 'connected' && targetChain) {
+        await switchChain(targetChain)
+      }
+
+      console.log(targetChain)
+
+      if (targetChain) {
+        setLastChainId(targetChain.id)
       }
     }
+
     handleChainChange()
-  }, [status, disabled])
+  }, [status, disabled, loading, baseNft, polygonNft])
 
   useEffectCompare(() => {
     if (disabled) return
     setLastChainId(chain.id)
     window.location.reload()
   }, [chain.id, disabled])
+
+  return { conflict }
 }
 
 export default useHandleSwitchChain
