a95e09b6e91dbb0a5472dfd5cea0b4ed9eb62b8c
basya01 <arsen@swarm.com>
2025-09-26 14:05:01 +0300

    feat: change Protofire subgraph to TheGraph

--- src/environment/networks/polygon.ts
+++ src/environment/networks/polygon.ts
@@ -3,8 +3,7 @@ import { Chain, polygon } from 'thirdweb/chains'
 
 export const polygonEnvironment: NetworkConfiguration = Object.freeze({
   subgraphURL:
-    'https://proxy.polygon.chain.love/subgraphs/name/polygon/chainwiki-polygon',
-  apiKey: 'f9tEp28A9xi7WP8hYoD11jSmeAnkwaAK9755i5NqoUE=',
+    'https://gateway.thegraph.com/api/subgraphs/id/CetXtg7uqLw1SPgzAF5fnyifsrk5uYcxw8Vm3bEbtpCy',
   contracts: {
     sx1155NFTFactoryAddress: '0x9bfF9401F1807cDC9DcF48E67869Cf555244cE7C',
   },
