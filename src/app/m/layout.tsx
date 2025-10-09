c49e59eb828d2eb0f49a87941e1f340bcf5468a4
basya01 <arsen@swarm.com>
2025-10-09 22:23:50 +0300

    fix: layout

--- src/app/m/layout.tsx
+++ src/app/m/layout.tsx
@@ -1,7 +1,18 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
 import Layout from 'src/components/common/Layout'
 import NftLayout from 'src/components/common/Layout/NftLayout'
+import Routes from 'src/shared/consts/routes'
 
 const ManagerLayout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname()
+  const isHomePage = pathname === Routes.manager.home
+
+  if (isHomePage) {
+    return <Layout>{children}</Layout>
+  }
+
   return (
     <Layout>
       <NftLayout>{children}</NftLayout>
