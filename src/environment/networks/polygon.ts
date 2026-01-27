4426990ad508510cfdd77325f8555f0fb46654e9
Arsen <arsen@swarm.com>
2026-01-28 00:51:46 +0200

    feat: update subgraph url

--- src/environment/networks/polygon.ts
+++ src/environment/networks/polygon.ts
@@ -3,7 +3,7 @@ import { Chain, polygon } from 'thirdweb/chains'
 
 export const polygonEnvironment: NetworkConfiguration = Object.freeze({
   subgraphURL:
-    'https://gateway.thegraph.com/api/subgraphs/id/CetXtg7uqLw1SPgzAF5fnyifsrk5uYcxw8Vm3bEbtpCy',
+    'https://api.subgraph.ormilabs.com/api/public/da826f4f-39ce-4acf-b4dd-7f36c948655e/subgraphs/chainwiki-polygon/v0.0.1/gn',
   contracts: {
     sx1155NFTFactoryAddress: '0x9bfF9401F1807cDC9DcF48E67869Cf555244cE7C',
     sx1155NFTImplementationAddress:
