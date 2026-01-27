ec1141b9ef10bd63511fd14ff428d5df8f4165d1
Arsen <arsen@swarm.com>
2026-01-27 16:14:15 +0200

    fix: naming

--- src/environment/index.ts
+++ src/environment/index.ts
@@ -7,5 +7,5 @@ export const environment = Object.freeze({
   thirdWebClientId: process.env.NEXT_PUBLIC_THIRD_WEB_CLIENT_ID as string,
   subgraphApiKey: process.env.NEXT_PUBLIC_SUBGRAPH_API_KEY as string,
   subgraphDevApiKey: process.env.NEXT_PUBLIC_DEV_SUBGRAPH_API_KEY,
-  girhubToken: process.env.NEXT_PUBLIC_GITHUB_PERSONAL_ACCESS_TOKEN as string,
+  githubToken: process.env.NEXT_PUBLIC_GITHUB_PERSONAL_ACCESS_TOKEN as string,
 })
